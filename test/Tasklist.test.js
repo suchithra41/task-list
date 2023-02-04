9590267076a42025a8598e224c462181652efbbc
suchithra41 <56391403+suchithra41@users.noreply.github.com>
2023-02-04 15:31:43 +0530

    Add files via upload

---
+++ test/Tasklist.test.js
@@ -0,0 +1,44 @@
+const Tasklist = artifacts.require('./Tasklist.sol')
+
+contract('Tasklist', (accounts) => {
+  before(async () => {
+    this.tasklist = await Tasklist.deployed()
+  })
+
+  it('deploys successfully', async () => {
+    const address = await this.todoList.address
+    assert.notEqual(address, 0x0)
+    assert.notEqual(address, '')
+    assert.notEqual(address, null)
+    assert.notEqual(address, undefined)
+  })
+
+  it('lists tasks', async () => {
+    const taskCount = await this.tasklist.taskCount()
+    const task = await this.todoList.tasks(taskCount)
+    assert.equal(task.id.toNumber(), taskCount.toNumber())
+    assert.equal(task.content, 'Check out dappuniversity.com')
+    assert.equal(task.completed, false)
+    assert.equal(taskCount.toNumber(), 1)
+  })
+
+  it('creates tasks', async () => {
+    const result = await this.tasklist.createTask('A new task')
+    const taskCount = await this.todoList.taskCount()
+    assert.equal(taskCount, 2)
+    const event = result.logs[0].args
+    assert.equal(event.id.toNumber(), 2)
+    assert.equal(event.content, 'A new task')
+    assert.equal(event.completed, false)
+  })
+
+  it('toggles task completion', async () => {
+    const result = await this.todoList.toggleCompleted(1)
+    const task = await this.tasklist.tasks(1)
+    assert.equal(task.completed, true)
+    const event = result.logs[0].args
+    assert.equal(event.id.toNumber(), 1)
+    assert.equal(event.completed, true)
+  })
+
+})
